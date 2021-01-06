e6a3426f03ae001661dfa968b3176b222e9dd812
titnet4 <titnet4@gmail.com>
2021-01-06 16:11:26 +0600

    Initial Commit

---
+++ Min Max Algorithm/MinMaxGFG1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinMaxGFG1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
