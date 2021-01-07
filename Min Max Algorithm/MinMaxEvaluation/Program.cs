7811f673f34c09834d41ad6c824a0a1e5bbf0cf3
titnet4 <titnet4@gmail.com>
2021-01-07 15:56:19 +0600

    min max algorithm implementaion

---
+++ Min Max Algorithm/MinMaxEvaluation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MinMaxEvaluation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                char[,] board = {
+                { 'x', '_', 'o'},
+                { '_', 'x', 'o'},
+                { '_', '_', 'x'}
+            };
+
+            int val = tictactoeBoardEvaluation(board);
+            Console.WriteLine(val);
+        }
+
+        private static int tictactoeBoardEvaluation(char[,] b)
+        {
+            for (int row=0; row<3; row++)
+            {
+                if (b[row, 0] == b[row,1] && b[row,1] == b[row, 2])
+                {
+                    if (b[row, 0] == 'x')
+                        return 1;
+                    else if (b[row, 0] == 'o')
+                        return -1;
+                }
+            }
+
+            for (int col=0; col<3; col++)
+            {
+                if (b[0, col] == b[1, col] && b[1, col] == b[2, col])
+                {
+                    if (b[0, col] == 'x')
+                        return 1;
+                    else if (b[0, col] == 'o')
+                        return -1;
+                }
+            }
+
+            if (b[0, 0] == b[1, 1] && b[1, 1] == b[2, 2])
+            {
+                if (b[0, 0] == 'x')
+                    return +1;
+                else if (b[0, 0] == 'o')
+                    return -1;
+            }
+
+            if (b[0, 2] == b[1, 1] && b[1, 1] == b[2, 0])
+            {
+                if (b[0, 2] == 'x')
+                    return +1;
+                else if (b[0, 2] == 'o')
+                    return -1;
+            }
+
+            return 0;
+        }
+    }
+}
--- Min Max Algorithm/MinMaxGFG1/Program.cs
+++ Min Max Algorithm/MinMaxGFG1/Program.cs
@@ -6,7 +6,27 @@ namespace MinMaxGFG1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] scores = { 3, 5, 2, 9, 12, 5, 23, 23 };
+            int n = scores.Length;
+            int h = log2(n);
+            var val = minimax(0, 0, true, scores, h);
+            Console.WriteLine($"The h is {val}");
+        }
+
+        private static int log2(int n)
+        {
+            return (n == 1) ? 0 : 1 + log2(n/2);
+        }
+
+        private static int minimax(int depth, int nodeIndex, bool isMax, int []scores, int h)
+        {
+            if (depth == h)
+                return scores[nodeIndex];
+
+            if (isMax)
+                return Math.Max(minimax(depth + 1, nodeIndex * 2, false, scores, h), minimax(depth + 1, nodeIndex * 2 + 1, false, scores, h));
+            else
+                return Math.Min(minimax(depth + 1, nodeIndex * 2, true, scores, h), minimax(depth + 1, nodeIndex * 2 + 1, true, scores, h));
         }
     }
 }
